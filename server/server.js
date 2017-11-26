051ca85ca67f316cdf2ca40b322dbe082a73d28b
pejman <naderpejman@yahoo.com>
2017-11-26 16:23:06 -0700

    section 9:lec106_create new project
    setting up express and path
    Setting up package. jason for heruku  -->
    heroku create  ,
    git push heroku master

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path=require ('path');  //default node package no need to
+const express=require('express');
+
+const publicPath=path.join(__dirname, '../public' );// join takes the partial paths and joins them together
+const port=process.env.PORT || 3000; //herouku needs this
+var app=express();
+
+
+app.use(express.static(publicPath));
+
+app.listen(3000,()=>{
+  console.log('Server is up on port 3000');
+});
