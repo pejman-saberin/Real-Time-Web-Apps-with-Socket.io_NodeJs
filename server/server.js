7748bd4e14afbd939de8bec13db01aaf1fca9b66
pejman <naderpejman@yahoo.com>
2017-11-26 18:46:34 -0700

    Section9-107. Adding Socket.io to an App
    Adding socket.io to front end and the server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const path=require ('path');  //default node package no need to
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
 
 const publicPath=path.join(__dirname, '../public' );// join takes the partial paths and joins them together
 const port=process.env.PORT || 3000; //herouku needs this
 var app=express();
-
+var server=http.createServer(app);
+var io=socketIO(server); //now we cab accept socket connections
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+//io.on(); //this listen to events
+io.on('connection', (socket)=>{  //this event is created from the server
+  console.log('New user connected');
+
+  socket.on('disconnect',()=>{
+    console.log('User was Disconnected');
+  });
+}); //this listen to connection events like a new user connected
+
+
+
+
+
+server.listen(port,()=>{  //app.lister is same as ttp.createServer(app) it does this behind the seen
   console.log(`Server is up on ${port}` );
 });
